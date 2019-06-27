bab60d06a86c5349f70bb0fa4525e82f8f26b690
Sumapraja <irfansumapraja@gmail.com>
2019-06-27 15:42:48 +0700

    changing the filters

---
+++ DataContacts.js
@@ -0,0 +1,37 @@
+const myContacts = [
+    {
+        id: 1,
+        name: 'Awesome Avalon',
+        phone: '+1 111 101010',
+        email: 'Awesome@avalon.org',
+        present: true,
+    },
+    {
+        id: 2,
+        name: 'Brave  Bahamut',
+        phone: '+62 812 242424',
+        email: 'brave@bahamut.com',
+        present: true,
+    },
+    {
+        id: 3,
+        name: 'Curious Charizard',
+        phone: '+63 813 363636',
+        email: 'curious@charizard.dev',
+        present: false,
+    },
+    {
+        id: 4,
+        name: 'Daring Drogon',
+        phone: '+62 818 8955647',
+        email: 'daring@drogon.co',
+        present: true,
+    },
+    {
+        id: 5,
+        name: 'Eager Ender',
+        phone: '+65 815 55555',
+        email: 'eager@ender.web',
+        present: false,
+    }
+]
--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-const myContacts = [
-  {
-    id: 1,
-    name: 'Alpha Avalon',
-    phone: '+1 111 101010',
-    email: 'alpha@avalon.org',
-    favorite: true,
-    rating: 9,
-    tags: ['popular', 'cool']
-  },
-  {
-    id: 2,
-    name: 'Betty Brave',
-    phone: '+62 812 242424',
-    email: 'betty@braverian.com'
-  },
-  {
-    id: 3,
-    name: 'Gamma Gacurio',
-    phone: '+63 813 363636',
-    email: 'gamma@gacurio.dev'
-  }
-]
-
-// -----------------------------------------------------------------------------
-
 const showContacts = contacts => {
   for (let index = 0; index < contacts.length; index++) {
     const contact = contacts[index]
@@ -32,16 +6,17 @@ const showContacts = contacts => {
     )
   }
 }
+showContacts(myContacts)
 
 // -----------------------------------------------------------------------------
 
-const filterContacts = (contacts, minimumNameLength) => {
+const filterContacts = (contacts, ) => {
   let newContacts = []
 
   for (let index = 0; index < contacts.length; index++) {
     const contact = contacts[index]
 
-    if (contact.name.length >= minimumNameLength) {
+    if (contact.present == true) {
       newContacts.push(contact)
     }
   }
@@ -49,12 +24,6 @@ const filterContacts = (contacts, minimumNameLength) => {
   return newContacts
 }
 
-// -----------------------------------------------------------------------------
-
-showContacts(myContacts)
-
-// -----------------------------------------------------------------------------
-
-const filteredContacts = filterContacts(myContacts, 12)
+const filteredContacts = filterContacts(myContacts)
 
 showContacts(filteredContacts)
